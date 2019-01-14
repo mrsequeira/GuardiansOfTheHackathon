bcb879e2ce3730cdfb65fed63c2a3afc0e72ac8c
Alexandre Sequeira <alex-sequeira@hotmail.com>
2019-01-14 22:53:26 +0000

    Can you eat this Travis?

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.require_master_key = true
+  # NOT FOR NOW :) config.require_master_key = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
