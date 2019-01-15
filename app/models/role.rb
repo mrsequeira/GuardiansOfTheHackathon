c5d8ecda0f168f0a1b8816093c7ab3c7bb83506c
Henrique Bernardo <henribernardo@hotmail.com>
2019-01-15 22:03:43 +0000

    completed testing models

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,5 @@ class Role < ApplicationRecord
     has_many :user_roles
     has_many :users, :through => :user_roles
 
-    validates :name, presence:true, length: {minimum:4, maximum:15}
+    validates :name, presence:true, length: {minimum:4, maximum:25}
 end
