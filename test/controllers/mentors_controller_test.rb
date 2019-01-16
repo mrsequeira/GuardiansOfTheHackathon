4b0ef88f1cccc7809c3c13bd1cb160796a6aab7c
Miguel Silva <aluno18904@ipt.pt>
2019-01-16 23:48:41 +0000

    Criados os testes aos controllers

--- test/controllers/mentors_controller_test.rb
+++ test/controllers/mentors_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class MentorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @mentor = mentors(:one)
+  end
+
+  test "should get index" do
+    get mentors_url, as: :json
+    assert_response :success
+  end
+
+  test "should create mentor" do
+    assert_difference('Mentor.count') do
+      post mentors_url, params: { mentor: { "name_mentor": "José","vegan": true,"tshirt_size": "L","mentor_difficulties": "nenhuma","mentor_allergies": "Asma","user_id": users(:one).id,"theme_id": theme(:teste).id } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show mentor" do
+    get mentor_url(@mentor), as: :json
+    assert_response :success
+  end
+
+  test "should update mentor" do
+    patch mentor_url(@mentor), params: { mentor: {  "name_mentor": "José","vegan": true,"tshirt_size": "L","mentor_difficulties": "nenhuma","mentor_allergies": "Asma","user_id": users(:one).id,"theme_id": theme(:teste).id  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy mentor" do
+    assert_difference('Mentor.count', -1) do
+      delete mentor_url(@mentor), as: :json
+    end
+
+    assert_response 204
+  end
 end
--- test/controllers/themes_controller_test.rb
+++ test/controllers/themes_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class ThemesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @theme = themes(:one)
+  end
+
+  test "should get index" do
+    get themes_url, as: :json
+    assert_response :success
+  end
+
+  test "should create theme" do
+    assert_difference('Theme.count') do
+      post themes_url, params: { theme: { "name_theme": "teste", "description_theme": "teste" } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show theme" do
+    get theme_url(@theme), as: :json
+    assert_response :success
+  end
+
+  test "should update theme" do
+    patch theme_url(@theme), params: { theme: { "name_theme": "teste", "description_theme": "teste" } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy theme" do
+    assert_difference('Theme.count', -1) do
+      delete theme_url(@theme), as: :json
+    end
+
+    assert_response 204
+  end
 end
